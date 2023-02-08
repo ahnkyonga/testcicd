4ab0addd3e0676ba8ed3e75e485dc17fcd5135c8
ahnkyonga <ahnkyonga@gmail.com>
2023-02-08 12:05:02 +0900

    y

--- testcicd/src/test/java/com/example/demo/TestcicdApplicationTests.java
+++ testcicd/src/test/java/com/example/demo/TestcicdApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
-class TestcicdApplicationTests {
+class TestcicdApplicationTests { 
 
 	//@Test
 	void contextLoads() {
